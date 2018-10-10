d2a1d960c0c82e1dd7434478c8c806e13e5d575d
ismaelalvesgit <cearaismael1997@gmail.com>
2018-10-10 10:56:30 -0300

    atualizado

---
+++ README.md
@@ -0,0 +1,8 @@
+# app5
+
+testes do curso
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:app5/ui/home_page.dart';
+void main (){
+  runApp(MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
